a819bb0bc561489e0f076bd4d24bc904144562bd
fanfanfanhaitao <58022265+fanfanfanhaitao@users.noreply.github.com>
2020-08-28 14:39:15 +0800

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    clientId: 'efa87842c625b2657dfc', // github的clientId
-    clientSecret: '8db90fa85a8d55ca4d0d78b14e79e05746c2a116', // github的cclientSecret
+    clientId: '', // github的clientId
+    clientSecret: '', // github的cclientSecret
     msgKey: '', // 短信发送的key
     emailPass: '', // 发送邮件的pass
     scope: ['user'],
